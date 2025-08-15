6547e2070cfbc024e2dd53ca54630c1f060a5640
baglamishelin <baglamishelin34@gmail.com>
2025-08-15 09:37:19 +0300

    Update 10- 04Model.py

--- helin/10- 04Model.py
+++ helin/10- 04Model.py
@@ -729,7 +729,7 @@ def main():
         # Output settings
         output_dir="./embedded_outputs",
         hub_model_id="mrkswe/phi4-model07",  # Your model name
-        hf_token="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",
+        hf_token="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",
 
         # Embedded behavior - BU DAVRANIŞI GÖMER!
         task_prefix="Endpoint seçimi:",
@@ -777,5 +777,6 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
 
